36e2f20207212e10ebdf2d79a2c189aec67bc0a0
Ilyushenichka <227179@edu.fa.ru>
2025-03-27 15:54:37 +0300

    Delete Island_Game/Island_Game directory

--- Island_Game/Island_Game/src/Area.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Area {
-    List<LivingBeing> beings = new ArrayList<>();
-    int plantCount = 5;
-}
--- Island_Game/Island_Game/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.util.concurrent.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Island island = new Island(100, 20);
-        island.simulate();
-    }
-}
